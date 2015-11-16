d202e76e5866c76909f9527df9e3680b72344b0d
Guilherme Roberge Daleffe <falecomdaleffe@gmail.com>
2015-11-16 16:23:53 -0200

    Checkbox render provided

--- src/Daleffe/Phalcon/Forms/Element/CheckGroup.php
+++ src/Daleffe/Phalcon/Forms/Element/CheckGroup.php
@@ -6,5 +6,27 @@ class CheckGroup extends \Phalcon\Forms\Element
 {
     public function render($attributes = [])
     {
+        $checked = null;
+        $html = '';
+
+        if (is_array($attributes)) {
+            foreach ($attributes as $key => $value) {
+                $this->setAttribute($key, $value);
+            }
+        } else {
+            $attributes = $this->getAttributes();
+        }
+
+        foreach ($attributes['elements'] as $key => $value) {
+            if (is_array($this->getValue())) {
+                $checked = (array_search($key, $this->getValue()) === false) ? null : ' checked';
+            } else {
+                $checked = ($key == $this->getValue()) ? ' checked' : null;
+            }
+
+            $html .= '<input type="checkbox" id="' . $this->getName() . $key . '" name="' . $this->getName() . '[]' . '" value="' . $value . '"' . $checked . ' /> ' . $value . ' ';
+        }
+
+        return $html;
     }
 }
